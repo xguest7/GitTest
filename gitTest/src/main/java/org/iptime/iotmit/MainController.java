2f889349daa2381d79068daa01423a790cbe64e2
lookerz <onlookgertozip@gmail.com>
2020-12-30 11:51:01 +0900

    글자수 입력화면 완성

---
+++ gitTest/src/main/java/org/iptime/iotmit/MainController.java
@@ -0,0 +1,21 @@
+package org.iptime.iotmit;
+
+/* 글자수 세기 프로그램
+http://127.0.0.1/strcount
+글자박스나오고
+글입력하고
+개수세기 버튼을 누르면
+url이 유지되면서
+총글자는 몇글자입니다. 라고 나오도록 만들어봅시다. */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/strcount")
+	public void aa() {
+		//->strcount.jsp
+	}
+}
---
+++ gitTest/src/test/java/CodeTest/TestStrCount.java
@@ -0,0 +1,12 @@
+package CodeTest;
+
+import org.junit.Test;
+
+public class TestStrCount {
+
+	@Test
+	public void countTest() {
+		String str="이 글자는 몇글자 일까요?";
+		System.out.println(str.length());
+	}
+}
